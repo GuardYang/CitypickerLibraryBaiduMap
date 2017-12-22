6a146d02a698bf2acf0b0daf6eeea61eff2c8b3c
yangshirong <yangshirong@log56.com>
2017-12-22 14:50:23 +0800

    百度定位

--- citypickerlibrary/src/main/java/com/ysr/citypicker/CityPickerActivity.java
+++ citypickerlibrary/src/main/java/com/ysr/citypicker/CityPickerActivity.java
@@ -19,10 +19,10 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
-import com.amap.api.location.AMapLocation;
-import com.amap.api.location.AMapLocationClient;
-import com.amap.api.location.AMapLocationClientOption;
-import com.amap.api.location.AMapLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.ysr.citypicker.adapter.CityListAdapter;
 import com.ysr.citypicker.adapter.ResultListAdapter;
 import com.ysr.citypicker.db.DBManager;
@@ -50,8 +50,9 @@ public class CityPickerActivity extends AppCompatActivity implements View.OnClic
     private ResultListAdapter mResultAdapter;
     private List<City> mAllCities;
     private DBManager dbManager;
-
-    private AMapLocationClient mLocationClient;
+    public LocationClient mLocationClient = null;
+    private MyLocationListener myListener = new MyLocationListener();
+//    private AMapLocationClient mLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,31 +64,68 @@ public class CityPickerActivity extends AppCompatActivity implements View.OnClic
         initLocation();
     }
 
-    private void initLocation() {
-        mLocationClient = new AMapLocationClient(this);
-        AMapLocationClientOption option = new AMapLocationClientOption();
-        option.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
-        option.setOnceLocation(true);
-        mLocationClient.setLocationOption(option);
-        mLocationClient.setLocationListener(new AMapLocationListener() {
-            @Override
-            public void onLocationChanged(AMapLocation aMapLocation) {
-                if (aMapLocation != null) {
-                    if (aMapLocation.getErrorCode() == 0) {
-                        String city = aMapLocation.getCity();
-                        String district = aMapLocation.getDistrict();
-                        String location = StringUtils.extractLocation(city, district);
-                        mCityAdapter.updateLocateState(LocateState.SUCCESS, location);
-                    } else {
-                        //定位失败
-                        mCityAdapter.updateLocateState(LocateState.FAILED, null);
-                    }
-                }
-            }
-        });
-        mLocationClient.startLocation();
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mLocationClient.registerLocationListener(myListener);
     }
 
+    private void initLocation() {
+        mLocationClient = new LocationClient(getApplicationContext());
+        //声明LocationClient类
+
+        //注册监听函数
+        LocationClientOption mOption = new LocationClientOption();
+        mOption = new LocationClientOption();
+        mOption.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        mOption.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
+        mOption.setScanSpan(3000);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        mOption.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        mOption.setIsNeedLocationDescribe(true);//可选，设置是否需要地址描述
+        mOption.setNeedDeviceDirect(false);//可选，设置是否需要设备方向结果
+        mOption.setLocationNotify(false);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+        mOption.setIgnoreKillProcess(true);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        mOption.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        mOption.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        mOption.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+
+        mOption.setIsNeedAltitude(false);//可选，默认false，设置定位时是否需要海拔信息，默认不需要，除基础定位版本都可用
+        //可选，是否需要地址信息，默认为不需要，即参数为false
+        //如果开发者需要获得当前点的地址信息，此处必须为true
+
+        mLocationClient.setLocOption(mOption);
+        mLocationClient.start();
+    }
+    public class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location){
+            //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
+            //以下只列举部分获取地址相关的结果信息
+            //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
+
+//            String addr = location.getAddrStr();    //获取详细地址信息
+//            String country = location.getCountry();    //获取国家
+//            String province = location.getProvince();    //获取省份
+//            String city = location.getCity();    //获取城市
+//            String district = location.getDistrict();    //获取区县
+//            String street = location.getStreet();    //获取街道信息
+
+
+                if (null != location && location.getLocType() != BDLocation.TypeServerError) {
+                    String city = location.getCity();
+                    String district = location.getDistrict();
+                    String location_info = StringUtils.extractLocation(city, district);
+                    mCityAdapter.updateLocateState(LocateState.SUCCESS, location_info);
+                } else {
+                    //定位失败
+                    mCityAdapter.updateLocateState(LocateState.FAILED, null);
+
+            }
+
+
+
+        }
+    }
     private void initData() {
         dbManager = new DBManager(this);
         dbManager.copyDBFile();
@@ -102,7 +140,7 @@ public class CityPickerActivity extends AppCompatActivity implements View.OnClic
             @Override
             public void onLocateClick() {
                 mCityAdapter.updateLocateState(LocateState.LOCATING, null);
-                mLocationClient.startLocation();
+                mLocationClient.start();
             }
         });
 
@@ -194,7 +232,7 @@ public class CityPickerActivity extends AppCompatActivity implements View.OnClic
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mLocationClient.stopLocation();
+        mLocationClient.stop();
     }
 
     private void getPersimmions() {
